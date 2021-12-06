4ea5c9b99872ed2da6004d17fa64325f6a8172ce
Lindrit Krasniqi <linndritks@gmail.com>
2021-12-06 19:46:35 +0100

    Create send and update action

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,22 +2,11 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\User;
+use App\Http\Controllers\Auth\LoginController;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
 
 class HomeController extends Controller
 {
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        $this->middleware('auth');
-    }
-
     /**
      * Show the application dashboard.
      *
@@ -26,5 +15,41 @@ class HomeController extends Controller
     public function index()
     {
         return view('home');
+    }
+
+    /**
+     * Show the profile of authenticated user
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function show()
+    {
+        return view('auth.profile');
+    }
+
+    /**
+     * Update profile of authenticated user
+     *
+     * @param  \Illuminate\Http\Request $request
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function update(Request $request)
+    {
+        $request->validate([
+            'name' => ['required', 'string'],
+            'email' => ['required', 'string', 'email', 'exists:users,email']
+        ]);
+
+        auth()->user()->update([
+            'name' => $request->name,
+            'email' => $request->email
+        ]);
+
+        return back()->with(['message' => 'You have updated your profile data successfully!']);
+    }
+
+    public function lock()
+    {
+        return back()->with(['message' => 'Your account has been lock from now!']);
     }
 }
