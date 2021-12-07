d3de7497a5d027fd3a61b15c72f0db407f064085
Lindrit Krasniqi <linndritks@gmail.com>
2021-12-07 21:09:10 +0100

    Create RoleRequest::class for validation rules

---
+++ app/Http/Requests/RoleRequest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Models\Role;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class RoleRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return auth()->user()->role_id === Role::ADMINISTRATOR;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'role' => ['required', 'string', Rule::unique('roles')->ignore($this->route()->role)]
+        ];
+    }
+}
