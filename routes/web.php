0f7a82facb98e16538ddf502641d14bc100416c1
Lindrit Krasniqi <linndritks@gmail.com>
2021-12-08 13:51:31 +0100

    register new routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Auth\LockAccountController;
 use App\Http\Controllers\RoleController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Auth;
@@ -22,26 +23,40 @@ Route::get('/', function () {
 
 Auth::routes();
 
+/**
+ * Group of routes under auth middleware
+ */
 Route::group(['middleware' => ['auth']], function () {
     Route::get('/email/verify', [App\Http\Controllers\Auth\VerificationController::class, 'show'])->name('verification.notice');
     Route::get('/email/verify/{id}/{hash}', [App\Http\Controllers\Auth\VerificationController::class, 'verify'])->middleware(['signed'])->name('verification.verify');
     Route::post('/email/verification-notification', [App\Http\Controllers\Auth\VerificationController::class, 'send'])->middleware(['throttle:6,1'])->name('verification.send');
     Route::post('/email/resend/verification-notification', [App\Http\Controllers\Auth\VerificationController::class, 'resend'])->middleware(['throttle:6,1'])->name('verification.resend');
 
+    /**
+     * Group of routes under verified middleware
+     */
     Route::group(['middleware' => 'verified'], function () {
         Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
         Route::get('/profile', [App\Http\Controllers\HomeController::class, 'show'])->name('profile');
         Route::put('/profile', [App\Http\Controllers\HomeController::class, 'update'])->name('profile.update');
-        Route::view('/lock-account', 'auth.account-lock')->name('account');
-        Route::view('/sttings', 'auth.settings')->name('settings');
-        Route::post('/lock-account', [App\Http\Controllers\HomeController::class, 'lock'])->name('account.lock');
+        Route::view('/settings', 'auth.settings')->name('settings');
+        Route::view('/lock-account', 'auth.lock')->name('lock');
+        Route::delete('/lock-account', LockAccountController::class)->name('account.lock');
 
 
+        /**
+         * Group of routes under prefix "menage" named as 'menage.*'
+         */
         Route::group([
             'prefix' => 'menage',
             'as' => 'menage.'
         ], function () {
 
+
+            /** 
+             * Users routes
+             */
+            Route::get('users/trashed', [UserController::class, 'trashed'])->name('users.trashed');
             Route::resource('users', UserController::class)->names([
                 'index' => 'users',
                 'store' => 'users.store',
@@ -52,6 +67,12 @@ Route::group(['middleware' => ['auth']], function () {
                 'create' => 'users.create'
             ]);
             Route::put('users/{user}/change-password', [UserController::class, 'changePassword'])->name('users.change-password');
+            Route::put('users/{user}/restore', [UserController::class, 'restore'])->withTrashed()->name('users.restore');
+            Route::delete('users/{user}/delete', [UserController::class, 'delete'])->withTrashed()->name('users.delete');
+
+            /**
+             * Roles routes
+             */
             Route::resource('roles', RoleController::class)->names([
                 'index' => 'roles',
                 'store' => 'roles.store',
