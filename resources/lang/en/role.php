aaf3e64af02080254019c8b23f9894ebd60888d7
Lindrit Krasniqi <linndritks@gmail.com>
2021-12-10 21:29:47 +0100

    Create role language lines

---
+++ resources/lang/en/role.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Role Language Lines
+    |--------------------------------------------------------------------------
+    */
+
+    'stored' => 'The <b>:ROLE</b> role has been created.',
+    'updated' => 'The <b>:ROLE</b> role has been updated.',
+    'destroyed' => 'The <b>:ROLE</b> role has been removed permanently.',
+];
