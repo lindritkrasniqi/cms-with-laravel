51f096c8f3ca29a759d47438cbd4da4a0a5db7dc
Lindrit Krasniqi <linndritks@gmail.com>
2021-12-10 21:29:34 +0100

    Create user language lines

---
+++ resources/lang/en/user.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | User Language Lines
+    |--------------------------------------------------------------------------
+    */
+
+    'stored' => '<b>:Name</b> from now is new user.',
+    'updated' => '<b>:Name</b> profile has been updated.',
+    'destroyed' => 'Account of <b>:Name</b> is locked from now.',
+    'restored' => 'Account of <b>:Name</b> is unlocked from now.',
+    'deleted' => 'Account of <b>:Name</b> has been deleted permanently!',
+    'password' => 'The password account of <b>:Name</b> has been updated.',
+    'profile' => 'The profile has been updated.',
+];
