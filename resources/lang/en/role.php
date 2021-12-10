c392f5733d55ceda08e9478fa1bcab6914730e92
Lindrit Krasniqi <linndritks@gmail.com>
2021-12-10 21:53:42 +0100

    Add applied lenguage line

--- resources/lang/en/role.php
+++ resources/lang/en/role.php
@@ -11,4 +11,6 @@ return [
     'stored' => 'The <b>:ROLE</b> role has been created.',
     'updated' => 'The <b>:ROLE</b> role has been updated.',
     'destroyed' => 'The <b>:ROLE</b> role has been removed permanently.',
+    'applied' => 'The :Policy already is applied for the :ROLE role!',
+
 ];
