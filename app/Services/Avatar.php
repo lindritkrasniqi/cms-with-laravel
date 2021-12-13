feb2bb9fe8c352dd7b9ac00c07c17b300593f242
Lindrit Krasniqi <linndritks@gmail.com>
2021-12-13 16:15:41 +0100

    Create Avatar service class

---
+++ app/Services/Avatar.php
@@ -0,0 +1,125 @@
+<?php
+
+namespace App\Services;
+
+use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Facades\Storage;
+use Intervention\Image\Facades\Image;
+
+
+class Avatar
+{
+    /**
+     * Avatar disk space.
+     *
+     * @var string
+     */
+    private $disk = 'public';
+
+    /**
+     * Avatars directory.
+     *
+     * @var string
+     */
+    private $directory = 'avatars';
+
+    /**
+     * 1. Unlink previous avatar from disk.
+     * 2. Update the avatar record on database.
+     * 3. Store new avatar into disk.
+     *
+     * @param  \Illuminate\Http\UploadedFile $file
+     * @return void
+     */
+    public function update(UploadedFile $file): void
+    {
+        $avatar = request()->user()->avatar;
+
+        $this->delete($avatar->name . '.' . $avatar->extension);
+
+        $avatar->update($this->image($file));
+
+        $this->saveAs($file, $avatar->name . '.' . $avatar->extension);
+    }
+
+    /**
+     * Create an avatar on database and store in disk.
+     *
+     * @param  \Illuminate\Http\UploadedFile $file
+     * @return void
+     */
+    public function store(UploadedFile $file): void
+    {
+        $avatar = request()->user()->avatar()->create($this->image($file));
+
+        $this->saveAs($file, $avatar->name . '.' . $avatar->extension);
+    }
+
+    /**
+     * Destroy avatar for the current authenticated user.
+     *
+     * @return void
+     */
+    public function destroy(): void
+    {
+        $avatar = request()->user()->avatar;
+
+        $this->delete($avatar->name . '.' . $avatar->extension);
+
+        $avatar->delete();
+    }
+
+    /**
+     * Unlink avatar from disk.
+     *
+     * @param  string $avatar
+     * @return void
+     */
+    public function delete(string $avatar): void
+    {
+        Storage::disk($this->disk)->delete($this->directory . '/' . $avatar);
+    }
+
+    /**
+     * Store the avatar and make it square(fit).
+     *
+     * @param  \Illuminate\Http\UploadedFile $file
+     * @param  string $name
+     * @return void
+     */
+    private function saveAs(UploadedFile $file, string $name): void
+    {
+        $file->storeAs($this->directory, $name, $this->disk);
+
+        $this->makeAvatarFit($name);
+    }
+
+    /**
+     * Make any image file to size 512x512 as default.
+     *
+     * @param  string $avatar
+     * @param  int $size
+     * @return void
+     */
+    private function makeAvatarFit(string $avatar, int $size = 512): void
+    {
+        Image::make(storage_path("app/{$this->disk}/{$this->directory}/{$avatar}"))->fit($size, null, function ($constraint) {
+            $constraint->aspectRatio();
+        })->save();
+    }
+
+    /**
+     * Returns an array of \App\Models\Image::class attributes.
+     *
+     * @param  \Illuminate\Http\UploadedFile $file
+     * @return array
+     */
+    private function image(UploadedFile $file): array
+    {
+        return [
+            'name' => rand(10000, 99999999) . '_' . now()->timestamp,
+            'extension' => $file->getClientOriginalExtension(),
+            'size' => $file->getSize()
+        ];
+    }
+}
