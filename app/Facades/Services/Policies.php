6284d93e3c462e84cec62c998893ec7a6ba79479
Lindrit Krasniqi <linndritks@gmail.com>
2021-12-15 15:38:12 +0100

    Add doc blocks and return Contract instead of string

--- app/Facades/Services/Policies.php
+++ app/Facades/Services/Policies.php
@@ -2,12 +2,23 @@
 
 namespace App\Facades\Services;
 
+use App\Contracts\Services\IPolicies;
 use Illuminate\Support\Facades\Facade;
 
+/**
+ * @method static self of(\App\Models\User $user)
+ * @method static array all()
+ * @method static bool exists(string $policy)
+ * @method static bool hasRightOn(string $policy)
+ * @method static \App\Models\Premission policy(string $policy)
+ * @method static \Illuminate\Support\Collection getPremissions()
+ * 
+ * @source App\Contracts\Services\IPolicies - Policies Contract.
+ */
 class Policies extends Facade
 {
     public static function getFacadeAccessor()
     {
-        return 'policies';
+        return IPolicies::class;
     }
 }
