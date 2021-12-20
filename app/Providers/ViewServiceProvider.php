c2b3a4c393258af91196f0cae7d8a225d779c5d9
Lindrit Krasniqi <linndritks@gmail.com>
2021-12-20 19:11:03 +0100

    Register canAnyOf custom directive

--- app/Providers/ViewServiceProvider.php
+++ app/Providers/ViewServiceProvider.php
@@ -4,6 +4,8 @@ namespace App\Providers;
 
 use App\Facades\Repositories\Roles;
 use App\Facades\Services\Policy;
+use Illuminate\Support\Facades\Blade;
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
 
@@ -25,6 +27,34 @@ class ViewServiceProvider extends ServiceProvider
      * @return void
      */
     public function boot()
+    {
+        $this->registerDirectives();
+
+        $this->shareDataWithViews();
+    }
+
+    /**
+     * Register all custom directives.
+     *
+     * @return void
+     */
+    private function registerDirectives(): void
+    {
+        Blade::if('cananyof', function (array $abilities, array $models) {
+            foreach ($models as $model) {
+                if (Gate::any($abilities, $model)) return true;
+            }
+
+            return false;
+        });
+    }
+
+    /**
+     * Share data with views.
+     *
+     * @return void
+     */
+    private function shareDataWithViews(): void
     {
         View::composer('partials.roles.*', function ($view) {
             return $view->with(['roles' => Roles::roles()]);
